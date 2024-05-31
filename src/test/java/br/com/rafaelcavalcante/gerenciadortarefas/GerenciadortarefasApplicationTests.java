596c8222530b75730c7c544a610bb341f2cfc7d3
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-05-31 10:18:50 -0300

    Projeto Base

---
+++ src/test/java/br/com/rafaelcavalcante/gerenciadortarefas/GerenciadortarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelcavalcante.gerenciadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadortarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
