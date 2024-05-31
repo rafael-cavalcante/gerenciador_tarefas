141f76a9aedc3c0f3d8808e12a5f0f925395fbce
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-05-31 11:01:16 -0300

    Listar Tarefas Pendentes

--- src/main/java/br/com/rafaelcavalcante/gerenciadortarefas/controller/TarefaController.java
+++ src/main/java/br/com/rafaelcavalcante/gerenciadortarefas/controller/TarefaController.java
@@ -3,10 +3,9 @@ package br.com.rafaelcavalcante.gerenciadortarefas.controller;
 import br.com.rafaelcavalcante.gerenciadortarefas.model.Tarefa;
 import br.com.rafaelcavalcante.gerenciadortarefas.repository.TarefaRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -21,5 +20,10 @@ public class TarefaController {
     @Transactional
     public Tarefa adicionarTarefa(@RequestBody Tarefa tarefa) {
         return this.terefaRepository.save(tarefa);
+    }
+
+    @GetMapping("/pendentes")
+    public List<Tarefa> listarTarefasPendentes() {
+        return this.terefaRepository.findTop3ByPessoaIsNullOrderByPrazoAsc();
     }
 }
--- src/main/java/br/com/rafaelcavalcante/gerenciadortarefas/repository/TarefaRepository.java
+++ src/main/java/br/com/rafaelcavalcante/gerenciadortarefas/repository/TarefaRepository.java
@@ -3,5 +3,8 @@ package br.com.rafaelcavalcante.gerenciadortarefas.repository;
 import br.com.rafaelcavalcante.gerenciadortarefas.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+    List<Tarefa> findTop3ByPessoaIsNullOrderByPrazoAsc();
 }
